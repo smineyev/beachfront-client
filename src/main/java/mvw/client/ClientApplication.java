2c7f37ed17fa29764bb3d83a4652ff82aa195b05
Sergey Mineyev <sergey@androidworks.com>
2019-11-05 02:06:39 -0800

    gradle build configurable via properties

--- src/main/java/mvw/client/ClientApplication.java
+++ src/main/java/mvw/client/ClientApplication.java
@@ -13,14 +13,12 @@ import org.asynchttpclient.Response;
 public class ClientApplication {
 	
 	private static AtomicInteger responseCounter = new AtomicInteger(0);
+	private static AtomicInteger timeoutCounter = new AtomicInteger(0);
 	
-	public static void run(String serverHost, int qps) {
-		System.out.println("Server host: "+serverHost);
-		System.out.println("Target qps: "+qps);
-		
+	public static void run(String serverHost, int timeout, int qps) {
 		AsyncHttpClient httpClient = Dsl.asyncHttpClient();
 		BoundRequestBuilder getRequest = httpClient.prepareGet(
-				"http://"+serverHost+":8080/getDisplay?userid=1&displayid=2&timeout=3000");
+				"http://"+serverHost+":8080/getDisplay?userid=1&displayid=2&timeout="+timeout);
 		
 		long interval = 1000000*1000 / qps;
 		long nextTime = System.nanoTime() + interval;
@@ -29,14 +27,18 @@ public class ClientApplication {
 			    @Override
 			    public Object onCompleted(Response response) throws Exception {
 			    	int rNum = responseCounter.incrementAndGet();
-//			    	if (rNum%1000 == 0) {
-				    	System.out.print("#"+rNum+ ": "+ response.getStatusCode());
+			    	int tNUm;
+			    	if (response.getStatusCode() == 204) {
+			    		tNUm = timeoutCounter.incrementAndGet();
+			    	} else {
+			    		tNUm = timeoutCounter.get();
+			    	}
+			    	if (rNum%1000 == 0) {
+				    	System.out.println("#"+rNum+ ", timeouts: "+ tNUm);
 				    	if (response.getStatusCode() == 200) {
 				    		System.out.println(": "+response.getResponseBody());
-				    	} else {
-				    		System.out.println();
 				    	}
-//			    	}
+			    	}
 			        return response;
 			    }
 			});
@@ -51,13 +53,18 @@ public class ClientApplication {
 
 	public static void main(String[] args) throws Exception {
 		String serverHost = args[0];
-		int qps = Integer.parseInt(args[1]);
+		int timeout = Integer.parseInt(args[1]);
+		int qps = Integer.parseInt(args[2]);
 		
-		run(serverHost, qps);
+		System.out.println("Server host: "+serverHost);
+		System.out.println("Timeout: "+timeout);
+		System.out.println("Target qps: "+qps);
+		
+		run(serverHost, timeout, qps);
 //		int parallelism = ForkJoinPool.getCommonPoolParallelism();
 //		ForkJoinPool forkJoinPool = new ForkJoinPool(parallelism);
 //		for (int i=0; i<parallelism; i++) {
-//			forkJoinPool.submit(() -> run(serverHost, qps/parallelism));
+//			forkJoinPool.submit(() -> run(serverHost, timeout, qps/parallelism));
 //		}
 //		forkJoinPool.awaitTermination(1, TimeUnit.HOURS);
 	}
